f358df71bd45f1dec6f2608e12b6413e53c2590d
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-06-13 09:39:06 -0600

    I am just having a tiny problem with api cnnecting to the sever will be back for more research on it.

---
+++ backend/routes/professional.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const professionalController = require('../controllers/professional.js');
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/', professionalController.getData);
+// localhost:8080/professional/
+module.exports = router;
