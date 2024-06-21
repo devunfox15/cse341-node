00ef6c50a1787d7841c067d8a01178b17ba16feb
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-06-21 15:27:49 -0600

    we have a finished project

--- backend/routes/professional.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const professionalController = require('../controllers/professional.js');
-
-const router = express.Router();
-
-// GET /feed/posts
-router.get('/', professionalController.getData);
-// localhost:8080/professional/
-module.exports = router;
